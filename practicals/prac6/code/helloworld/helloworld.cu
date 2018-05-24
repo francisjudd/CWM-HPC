4e89b64e297cc33f397a896df38b123dbacdb663
Francis Judd <francis.judd@oriel.ox.ac.uk>
2018-05-24 15:12:18 +0100

    Update mid prac6

--- practicals/prac6/code/helloworld/helloworld.cu
+++ practicals/prac6/code/helloworld/helloworld.cu
@@ -18,6 +18,12 @@
 
 // write your kernel here
 
+__global__ void helloworld_GPU( void )
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  printf("Hello world from thread %d!\n",index);
+}
+
 //----------------------------------------------------------------------
 
 int main(void) {
@@ -30,9 +36,19 @@ int main(void) {
   // write you GPU initialization here
 
   //----------------------------------------------------------------------
+  // Initiate GPU 
+  int deviceId = 0;
+  int devCount;
 
-  
-  
+  // Get number of GPU available
+  cudaGetDeviceCount( &devCount );
+
+  // Check we have enough space
+  if( deviceId < devCount ) 
+  {
+    cudaSetDevice( deviceId );
+  } else return(1);
+
   //----------------------------------------------------------------------
   // TASK 3: execute your "Hello world" kernel on 1 block with 5 threads 
   //         using execution configuration syntax.
@@ -42,6 +58,14 @@ int main(void) {
   
   // execute your "Hello world" kernel here
   
+  int Gx=1, Gy=1, Gz=1;
+  int Bx=5, By=1, Bz=1;
+
+  dim3 Gd(Gx, Gy, Gz);
+  dim3 Bd(Bx, By, Bz);
+
+  helloworld_GPU<<< Gd, Bd >>>();
+
   //----------------------------------------------------------------------
  
   cudaDeviceReset(); 
