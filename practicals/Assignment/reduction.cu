5028ce90aa32529131fba786d6c23746a98ac6a3
Francis Judd <francis.judd@oriel.ox.ac.uk>
2018-05-25 16:59:55 +0100

    Final Update

--- practicals/Assignment/reduction.cu
+++ practicals/Assignment/reduction.cu
@@ -1,3 +1,6 @@
+//
+// Include the usual libraries
+//
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,30 +8,46 @@
 #include <float.h>
 
 
+//
+// Include cuda libraries
+//
+#include <cuda.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+
+// Define constants for the program (number of elements)
 #define NUM_ELS 1024
 
 __global__ void reduction(float *d_input, float *d_output)
 {
-    // Allocate shared memory
+  // Allocate shared memory
+  __shared__ float smem_array[NUM_ELS];
 
-    __shared__  float smem_array[NUM_ELS];
 
-    int tid = threadIdx.x;
+  // Get thread and global thread ID
+  int tid  = threadIdx.x;
+  int bid  = blockIdx.x;
+  int gtid = threadIdx.x + blockIdx.x * blockDim.x;
 
-    // first, each thread loads data into shared memory
+  // Loads data into shared memory
+  smem_array[tid] = d_input[gtid];
+  __syncthreads();
 
-    smem_array[tid] = d_input[tid];
 
-    // next, we perform binary tree reduction
-
-    for (int d = blockDim.x/2; d > 0; d /= 2) {
-      __syncthreads();  // ensure previous step completed 
-      if (tid<d)  smem_array[tid] += smem_array[tid+d];
+  // Binary Tree Reduction Per Block
+  for (int d = (blockDim.x)/2; d > 0; d /= 2) {
+    __syncthreads();  // ensure previous step completed 
+    if (tid<d)  {
+      smem_array[tid] += smem_array[tid + d];
     }
+  }
 
-    // finally, first thread puts result into global memory
-
-    if (tid==0) d_output[0] = smem_array[0];
+  // First thread puts result into global memory
+  if (tid==0) {
+     d_output[bid] = smem_array[0];
+  }
 }
 
 
@@ -39,56 +58,90 @@ __global__ void reduction(float *d_input, float *d_output)
 
 int main( int argc, const char** argv) 
 {
-    int num_els, num_threads, mem_size;
+    int num_els, i;
+    int num_threads, num_blocks, mem_size;
 
+    // Define  Host Memory Pointers
     float *h_data;
     float *d_input, *d_output;
 
-    // initialise card
+
+    // Initialise card
+    printf("Enter number of randoms required: ");
+    scanf("%d", &num_els);
 
 
-    num_els     = NUM_ELS;
-    num_threads = num_els;
-    mem_size    = sizeof(float) * num_els;
+    // Calculate remainder of num_els and assign extra blocks as required
+    div_t result  = div( num_els, NUM_ELS );
 
-    // allocate host memory to store the input data
-    // and initialize to integer values between 0 and 1000
-
-    h_data = (float*) malloc(mem_size);
-      
-    for(int i = 0; i < num_els; i++) {
-        h_data[i] = ((float)rand()/(float)RAND_MAX);
+    if( result.rem != 0 ) {
+      num_blocks  = result.quot + 1;
+    } else {
+      num_blocks  = result.quot;
     }
 
-    // allocate device memory input and output arrays
+    num_threads   = NUM_ELS;
+    mem_size      = sizeof(float) * num_els;
 
+
+    // Allocate host memory to store the input data
+    h_data  = (float*) malloc(mem_size);
+
+    // Initialize to integer values between 0 and 1000
+    /*for(int i = 0; i < num_els; i++) {
+        h_data[i] = 1.0f;
+    }*/
+
+
+    // Allocate device memory input and output arrays
     cudaMalloc((void**)&d_input, mem_size);
-    cudaMalloc((void**)&d_output, sizeof(float));
+    cudaMalloc((void**)&d_output, num_blocks*sizeof(float));
 
-    // copy host memory to device input array
+    
+    // Declare variables
+    curandGenerator_t gen;
 
-    cudaMemcpy(d_input, h_data, mem_size, cudaMemcpyHostToDevice);
+    // Create random number generator
+    curandCreateGenerator( &gen, CURAND_RNG_PSEUDO_DEFAULT );
 
-    // execute the kernel
+    // Set the generator options
+    curandSetPseudoRandomGeneratorSeed( gen, 1234ULL );
 
-    reduction<<<1,num_threads>>>(d_input,d_output);
+    // Generate the randoms!!
+    curandGenerateUniform( gen, d_input, num_els );
+    
+    // Copy host memory to device input array
+    cudaMemcpy(h_data, d_input, mem_size, cudaMemcpyDeviceToHost);
 
-    // copy result from device to host
+    for(i = 0; i < num_els;i++) {
+      printf("h_data[%d] = %f\n",i,h_data[i]);
+    }
 
-    cudaMemcpy(h_data, d_output, sizeof(float), cudaMemcpyDeviceToHost);
+    // Execute the kernel
+    reduction<<< num_blocks, num_threads>>>(d_input,d_output);
 
-    // check results
 
-    printf("reduction error = %f\n",h_data[0]/NUM_ELS);
+    // Copy result from device to host
+    cudaMemcpy(h_data, d_output, num_blocks*sizeof(float), cudaMemcpyDeviceToHost);
 
-    // cleanup memory
+    
+    // Sum reductions from each block
+    for( i = 1; i < num_blocks; i++ ) {
+      h_data[0] += h_data[i];
+    }
 
+    // Check results
+    printf("Reduction error = %f\n",h_data[0]/num_els);
+
+
+    // Cleanup memory
+    curandDestroyGenerator(gen);
     free(h_data);
     cudaFree(d_input);
     cudaFree(d_output);
 
-    // CUDA exit -- needed to flush printf write buffer
 
+    // CUDA exit -- needed to flush printf write buffer
     cudaDeviceReset();
 }
 
